9f67aa6f352958ceb6ea4f2c78bc56ba66bbf8e4
AlyszaZealand <als012@edu.zealand.dk>
2026-05-05 12:09:10 +0200

    Initial commit

---
+++ src/main/java/feedback/deckforge/DeckForgeApplication.java
@@ -0,0 +1,13 @@
+package feedback.deckforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeckForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeckForgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/feedback/deckforge/DeckForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package feedback.deckforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeckForgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
