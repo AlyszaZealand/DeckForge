a2e9f524ac466615eee09e50b60daef610c9c672
LauritzPetersen <lauritzpetersen1@gmail.com>
2026-05-17 15:44:09 +0200

    push

---
+++ src/main/java/feedback/deckforge/Exceptions/EventFullException.java
@@ -0,0 +1,7 @@
+package feedback.deckforge.Exceptions;
+
+public class EventFullException extends RuntimeException {
+    public EventFullException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/feedback/deckforge/Exceptions/InvalidEventSizeException.java
@@ -0,0 +1,7 @@
+package feedback.deckforge.Exceptions;
+
+public class InvalidEventSizeException extends RuntimeException {
+    public InvalidEventSizeException(String message) {
+        super(message);
+    }
+}
